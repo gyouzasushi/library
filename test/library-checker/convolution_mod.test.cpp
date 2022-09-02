3711cecea30731bd22a74af64edebaaf20866d6c
gyouzasushi <tasukuishikawa@gmail.com>
2022-09-03 01:43:57 +0900

    modify modint & add test of convolution

---
+++ test/library-checker/convolution_mod.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
+#include <iostream>
+
+#include "../../math/convolution.hpp"
+using mint = modint::modint998244353;
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<mint> a(n), b(m);
+    for (int i = 0; i < n; i++) std::cin >> a[i];
+    for (int i = 0; i < m; i++) std::cin >> b[i];
+    std::vector<mint> c = convolution(a, b);
+    for (int i = 0; i <= (n - 1) + (m - 1); i++) {
+        std::cout << c[i] << " \n"[i == (n - 1) + (m - 1)];
+    }
+}
