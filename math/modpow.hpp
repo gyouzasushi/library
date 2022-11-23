c41a5cc92c14d52990f2b7fc12e4163097009cd1
gyouzasushi <tasukuishikawa@gmail.com>
2022-11-23 17:59:37 +0900

    add integer_geometry

--- math/modpow.hpp
+++ math/modpow.hpp
@@ -33,21 +33,3 @@ long long modpow(long long a, long long n, long long m) {
     }
     return ret;
 }
-
-// todo!! returns the minimum k s.t. x^k == y (mod m)
-long long modlog(long long x, long long y, long long m) {
-    auto modlog_coprime = [](long long x, long long y,
-                             long long m) -> long long {
-        if (x == 0) {
-            if (y == 1) return 0;
-            if (y == 0) return 1;
-            return -1;
-        }
-        const long long p = (long long)std::sqrt(x) + 2;
-        std::unordered_map<long long, long long> mp;
-        for (long long i = 0, e = 1; i < p; i++) {
-            mp[e] = i;
-            (e *= x) %= m;
-        }
-    };
-}
