b9421d123b57c11a77721741ff31379ce328f759
gyouzasushi <tasukuishikawa@gmail.com>
2024-06-29 11:05:42 +0900

    gyouza

---
+++ test/yukicoder/1601_2.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://yukicoder.me/problems/1601"
+
+#include <iostream>
+
+#include "datastructure/range_map.hpp"
+int main() {
+    long long d;
+    int q;
+    std::cin >> d >> q;
+    range_map<long long, bool> st;
+    long long ans = 0;
+    while (q--) {
+        long long a, b;
+        std::cin >> a >> b;
+        st.set(
+            {a, b}, true,
+            [&](long long l, long long r) { ans = std::max(ans, r - l + 1); },
+            [](long long, long long) {});
+        std::cout << ans << '\n';
+    }
+}
